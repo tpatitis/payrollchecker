010277f776a146d803d456a196a522b8fc8bcebe
tpatitis <162544571+tpatitis@users.noreply.github.com>
2026-05-07 21:36:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ elif page == "2. Γενικά Παραδοτέα (Checklist)":
     ]
 
     # Επιλογές κατάστασης
-    options = ["Λείπει", "Πλήρες", "Δεν απαιτείται"]
+    options = ["Έλλειψη", "Υπάρχει", "Δεν απαιτείται"]
     
     col_left, col_right = st.columns(2)
     half = len(required_docs) // 2
