8bc6ec691f876c9c53c7f97ee89b3a7147dc8c7d
tpatitis <162544571+tpatitis@users.noreply.github.com>
2026-05-29 21:00:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, Field
 # --- 1. ΡΥΘΜΙΣΗ ΣΕΛΙΔΑΣ ---
 st.set_page_config(
     page_title="Payroll Verifier Pro",
-    page_icon="🛡️",
+    page_icon="🛡️",
     layout="wide"
 )
 
